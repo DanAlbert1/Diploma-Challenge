05c8f56571206500e6e65b7b6c8d9042f933bb21
Daniel <danielalbert3377@gmail.com>
2022-06-26 18:41:56 +1000

    Changed naming of dist output

--- FE/karma.conf.js
+++ FE/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fe-diploma-challenge'),
+      dir: require('path').join(__dirname, './coverage/FE'),
       subdir: '.',
       reporters: [
         { type: 'html' },
